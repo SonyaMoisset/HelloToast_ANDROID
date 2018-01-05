f511c791a32ed4201be0dff57f3a59f1c8772430
smoisset <smoisset@worldremit.com>
2018-01-05 00:46:20 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/hellotoast/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, getString(R.string.toast_message), Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
